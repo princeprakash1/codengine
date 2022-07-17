e5ee7e30b911a290a7b1b8a54abb1885a832d2eb
Prince Dangi <princeprakash100@gmail.com>
2022-07-17 21:29:31 +0530

    dev

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log("This line is from first commit");
 console.log("This line is from second commit");
+
+console.log("This line is from new branch");
+console.log("This line is from new commit message");
